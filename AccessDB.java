1591bd1cfb432928bb28e36b039ca3b080aa83f1
Christian De Leon <145353689+lChristl@users.noreply.github.com>
2024-05-27 13:38:43 +0800

    upd 27/05/2024

---
+++ AccessDB.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AccessDB {
+
+	public static Connection conn() {
+		try {
+			String url = "jdbc:ucanaccess://Database-for-student-portal.accdb";
+			Connection conn = DriverManager.getConnection(url);
+			return conn;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
