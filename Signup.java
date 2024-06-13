47e1a7444e303096a5580ee9348cfb350411a558
Christian De Leon <145353689+lChristl@users.noreply.github.com>
2024-06-14 07:16:53 +0800

    upd

--- Signup.java
+++ Signup.java
@@ -8,6 +8,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -22,25 +25,18 @@ public class Signup extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField_11;
-	private JTextField textField_12;
-	private JTextField textField_13;
-	private JTextField textField_14;
-	private JTextField textField_15;
+	private JTextField f1;
+	private JTextField f2;
+	private JTextField f3;
+	private JTextField f4;
+	private JTextField f5;
+	private JTextField f6;
+	private JTextField f7;
+	private JTextField f8;
 	private JButton btnNewButton;
 	private JButton btnNewButton_1;
 	private int mouseX, mouseY;
+	Connection conn;
 
 	/**
 	 * Launch the application.
@@ -64,9 +60,9 @@ public class Signup extends JFrame {
 	public Signup() {
 
 		setResizable(false);
-
+		conn = AccessDB.conn();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1088, 675);
+		setBounds(100, 100, 1088, 437);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 27, 70, 220));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -74,19 +70,19 @@ public class Signup extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Last Name");
+		JLabel lblNewLabel = new JLabel("Username");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setBounds(63, 127, 185, 14);
 		lblNewLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("First Name");
+		JLabel lblNewLabel_1 = new JLabel("Password");
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setBounds(63, 190, 185, 14);
 		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_1);
 
-		JLabel lblNewLabel_2 = new JLabel("Middle Name");
+		JLabel lblNewLabel_2 = new JLabel("Full Name (F.MI.L)");
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		lblNewLabel_2.setBounds(63, 253, 185, 14);
 		lblNewLabel_2.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
@@ -98,179 +94,116 @@ public class Signup extends JFrame {
 		lblNewLabel_3.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_3);
 
-		JLabel lblNewLabel_4 = new JLabel("Strand");
-		lblNewLabel_4.setForeground(new Color(255, 255, 255));
-		lblNewLabel_4.setBounds(63, 379, 185, 14);
-		lblNewLabel_4.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_4);
-
-		JLabel lblNewLabel_5 = new JLabel("Date Of Birth");
-		lblNewLabel_5.setForeground(new Color(255, 255, 255));
-		lblNewLabel_5.setBounds(63, 442, 185, 14);
-		lblNewLabel_5.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_5);
-
-		JLabel lblNewLabel_6 = new JLabel("Gender");
-		lblNewLabel_6.setForeground(new Color(255, 255, 255));
-		lblNewLabel_6.setBounds(63, 505, 185, 14);
-		lblNewLabel_6.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_6);
-
-		JLabel lblNewLabel_7 = new JLabel("Citizenship");
-		lblNewLabel_7.setForeground(new Color(255, 255, 255));
-		lblNewLabel_7.setBounds(63, 568, 185, 14);
-		lblNewLabel_7.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_7);
-
-		JLabel lblNewLabel_8 = new JLabel("Contact Number");
+		JLabel lblNewLabel_8 = new JLabel("Strand");
 		lblNewLabel_8.setForeground(new Color(255, 255, 255));
 		lblNewLabel_8.setBounds(535, 127, 185, 14);
 		lblNewLabel_8.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_8);
 
-		JLabel lblNewLabel_9 = new JLabel("Email Address");
+		JLabel lblNewLabel_9 = new JLabel("Section");
 		lblNewLabel_9.setForeground(new Color(255, 255, 255));
 		lblNewLabel_9.setBounds(535, 190, 185, 14);
 		lblNewLabel_9.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_9);
 
-		JLabel lblNewLabel_10 = new JLabel("Address");
+		JLabel lblNewLabel_10 = new JLabel("School Year");
 		lblNewLabel_10.setForeground(new Color(255, 255, 255));
 		lblNewLabel_10.setBounds(535, 253, 185, 14);
 		lblNewLabel_10.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_10);
 
-		JLabel lblNewLabel_11 = new JLabel("Guardian");
+		JLabel lblNewLabel_11 = new JLabel("Contact Number");
 		lblNewLabel_11.setForeground(new Color(255, 255, 255));
 		lblNewLabel_11.setBounds(535, 316, 185, 14);
 		lblNewLabel_11.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_11);
 
-		JLabel lblNewLabel_12 = new JLabel("Relationship");
-		lblNewLabel_12.setForeground(new Color(255, 255, 255));
-		lblNewLabel_12.setBounds(535, 379, 185, 14);
-		lblNewLabel_12.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_12);
+		f1 = new JTextField();
+		f1.setBounds(263, 124, 262, 20);
+		f1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		contentPane.add(f1);
+		f1.setColumns(10);
 
-		JLabel lblNewLabel_13 = new JLabel("Contact Number");
-		lblNewLabel_13.setForeground(new Color(255, 255, 255));
-		lblNewLabel_13.setBounds(535, 442, 185, 14);
-		lblNewLabel_13.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_13);
+		f2 = new JTextField();
+		f2.setBounds(263, 187, 262, 20);
+		f2.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		contentPane.add(f2);
+		f2.setColumns(10);
 
-		JLabel lblNewLabel_14 = new JLabel("Previous School Attended");
-		lblNewLabel_14.setForeground(new Color(255, 255, 255));
-		lblNewLabel_14.setBounds(535, 505, 185, 14);
-		lblNewLabel_14.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_14);
+		f3 = new JTextField();
+		f3.setBounds(263, 250, 262, 20);
+		f3.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		contentPane.add(f3);
+		f3.setColumns(10);
 
-		textField = new JTextField();
-		textField.setBounds(263, 124, 262, 20);
-		textField.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField);
-		textField.setColumns(10);
+		f4 = new JTextField();
+		f4.setBounds(263, 313, 262, 20);
+		f4.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		contentPane.add(f4);
+		f4.setColumns(10);
 
-		textField_1 = new JTextField();
-		textField_1.setBounds(263, 187, 262, 20);
-		textField_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		f5 = new JTextField();
+		f5.setBounds(735, 124, 262, 20);
+		f5.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		f5.setColumns(10);
+		contentPane.add(f5);
 
-		textField_2 = new JTextField();
-		textField_2.setBounds(263, 250, 262, 20);
-		textField_2.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		f6 = new JTextField();
+		f6.setBounds(735, 187, 262, 20);
+		f6.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		f6.setColumns(10);
+		contentPane.add(f6);
 
-		textField_3 = new JTextField();
-		textField_3.setBounds(263, 313, 262, 20);
-		textField_3.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		f7 = new JTextField();
+		f7.setBounds(735, 250, 262, 20);
+		f7.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		f7.setColumns(10);
+		contentPane.add(f7);
 
-		textField_4 = new JTextField();
-		textField_4.setBounds(263, 376, 262, 20);
-		textField_4.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
-
-		textField_5 = new JTextField();
-		textField_5.setBounds(263, 439, 262, 20);
-		textField_5.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_5);
-		textField_5.setColumns(10);
-
-		textField_6 = new JTextField();
-		textField_6.setBounds(263, 502, 262, 20);
-		textField_6.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_6);
-		textField_6.setColumns(10);
-
-		textField_7 = new JTextField();
-		textField_7.setBounds(263, 565, 262, 20);
-		textField_7.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_7.setColumns(10);
-		contentPane.add(textField_7);
-
-		textField_8 = new JTextField();
-		textField_8.setBounds(735, 124, 262, 20);
-		textField_8.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_8.setColumns(10);
-		contentPane.add(textField_8);
-
-		textField_9 = new JTextField();
-		textField_9.setBounds(735, 187, 262, 20);
-		textField_9.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_9.setColumns(10);
-		contentPane.add(textField_9);
-
-		textField_10 = new JTextField();
-		textField_10.setBounds(735, 250, 262, 20);
-		textField_10.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_10.setColumns(10);
-		contentPane.add(textField_10);
-
-		textField_11 = new JTextField();
-		textField_11.setBounds(735, 313, 262, 20);
-		textField_11.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_11.setColumns(10);
-		contentPane.add(textField_11);
-
-		textField_12 = new JTextField();
-		textField_12.setBounds(735, 376, 262, 20);
-		textField_12.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_12.setColumns(10);
-		contentPane.add(textField_12);
-
-		textField_13 = new JTextField();
-		textField_13.setBounds(735, 439, 262, 20);
-		textField_13.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_13.setColumns(10);
-		contentPane.add(textField_13);
-
-		textField_14 = new JTextField();
-		textField_14.setBounds(735, 502, 262, 20);
-		textField_14.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		textField_14.setColumns(10);
-		contentPane.add(textField_14);
-
-		JLabel lblNewLabel_15 = new JLabel("School Address");
-		lblNewLabel_15.setForeground(new Color(255, 255, 255));
-		lblNewLabel_15.setBounds(535, 568, 185, 14);
-		lblNewLabel_15.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(lblNewLabel_15);
-
-		textField_15 = new JTextField();
-		textField_15.setBounds(735, 565, 262, 20);
-		textField_15.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		contentPane.add(textField_15);
-		textField_15.setColumns(10);
+		f8 = new JTextField();
+		f8.setBounds(735, 313, 262, 20);
+		f8.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
+		f8.setColumns(10);
+		contentPane.add(f8);
 
 		btnNewButton = new JButton("Register");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Statement st = conn.createStatement();
+					String name = f1.getText();
+					String password = f2.getText();
+					String s_name = f3.getText();
+					String s_number = f4.getText();
+					String strand = f5.getText();
+					String section = f6.getText();
+					String sy = f7.getText();
+					String contact = f8.getText();
+
+					String query = "INSERT INTO Userlogin(user_name, user_password, student_name, student_number, "
+							+ "strand, section, school_year, contact_number) VALUES ('" + name + "', '" + password
+							+ "', '" + s_name + "', '" + s_number + "','" + strand + "','" + section + "', '" + sy
+							+ "', '" + contact + "')";
+					System.out.println("Executing query: " + query);
+
+					int rowsAffected = st.executeUpdate(query);
+
+					if (rowsAffected > 0) {
+						System.out.println("Insert successful.");
+					} else {
+						System.out.println("Insert failed.");
+					}
+
+					st.close(); // Closing the Statement
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(0, 27, 70));
 		btnNewButton.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-		btnNewButton.setBounds(826, 631, 171, 33);
+		btnNewButton.setBounds(826, 371, 171, 33);
 		contentPane.add(btnNewButton);
 
 		JPanel navypanel1 = new JPanel();
@@ -321,7 +254,7 @@ public class Signup extends JFrame {
 		btnCancel.setForeground(Color.WHITE);
 		btnCancel.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
 		btnCancel.setBackground(new Color(0, 27, 70));
-		btnCancel.setBounds(644, 631, 171, 33);
+		btnCancel.setBounds(644, 371, 171, 33);
 		contentPane.add(btnCancel);
 
 		// *******************************||SET-ROUND-RECTANGLE-TO-PANEL||**********************************************//
